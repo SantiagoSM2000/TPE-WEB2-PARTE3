6afa42e7d0d9f2480e56872b4b48c35ef59d15f3
SantiagoSM2000 <111095204+SantiagoSM2000@users.noreply.github.com>
2024-11-17 23:47:53 -0300

    Add files via upload

--- api-rest/app/models/Model.php
+++ api-rest/app/models/Model.php
@@ -32,11 +32,30 @@ class Model {
             `Room_number` int(11) NOT NULL,
             `Image` varchar(300) DEFAULT NULL,
             `ID_Client` int(11) NOT NULL
+            `Payed` tinyint(1) NOT NULL
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-        INSERT INTO `reservations` (`ID_Reservation`, `Date`, `Room_number`, `Image`, `ID_Client`) VALUES
+        INSERT INTO `reservations` (`ID_Reservation`, `Date`, `Room_number`, `Image`, `ID_Client`, `Payed`) VALUES
         (29, '2024-10-20', 202, 'https://www.cataloniahotels.com/es/blog/wp-content/uploads/2024/01/catalonia-las-vegas-habitacion-doble-con-balcon-620x413.jpg', 1),
-        (31, '2024-10-20', 303, '', 5);
+        (38, '2025-02-19', 130, ' ', 5, 1),
+        (39, '2024-10-20', 102, '1', 1, 0),
+        (41, '2024-10-20', 202, '1', 1, 1),
+        (42, '2024-03-04', 545, '1', 5, 0),
+        (43, '2024-10-20', 345, '1', 1, 0),
+        (44, '2024-11-12', 12, 'https://www.cataloniahotels.com/es/blog/wp-content/uploads/2024/01/catalonia-las-vegas-habitacion-doble-con-balcon-620x413.jpg', 5, 1),
+        (45, '2024-04-15', 325, '1', 1, 1),
+        (46, '2024-06-11', 223, '1', 1, 0),
+        (47, '2024-10-20', 23, '1', 5, 0),
+        (48, '2024-10-20', 78, '1', 1, 1),
+        (49, '2025-02-28', 15, '1', 5, 0),
+        (50, '2024-10-20', 202, '1', 1, 1),
+        (51, '2024-10-20', 202, '1', 5, 0),
+        (65, '2024-10-20', 202, NULL, 1, 0),
+        (66, '2024-10-20', 202, ' ', 1, 0),
+        (67, '2024-10-20', 202, 'http', 1, 0),
+        (68, '2024-10-20', 202, ' ', 1, 0),
+        (69, '2025-02-19', 125, '1', 1, 1),
+        (71, '2025-01-10', 132, ' ', 5, 1);
 
         CREATE TABLE `users` (
             `ID_User` int(11) NOT NULL,
